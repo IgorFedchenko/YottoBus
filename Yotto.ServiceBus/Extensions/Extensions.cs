67abd3cf6ad3ab4d112891bc535de0103ec9d989
IgorFedchenko <igor@fedchenko.pro>
2017-01-17 03:03:48 +0300

    Added extensions for peer naming

--- Yotto.ServiceBus/Extensions/Extensions.cs
+++ Yotto.ServiceBus/Extensions/Extensions.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Yotto.ServiceBus.Abstract;
 using Yotto.ServiceBus.Configuration;
+using Yotto.ServiceBus.Model;
 
 namespace Yotto.ServiceBus.Extensions
 {
@@ -13,6 +14,35 @@ namespace Yotto.ServiceBus.Extensions
         public static IPeer CreatePeer(this IServiceBus bus)
         {
             return bus.CreatePeer(new PeerConfiguration());
+        }
+
+        public static IPeer CreatePeer(this IServiceBus bus, string peerName)
+        {
+            return bus.CreatePeer(new PeerConfiguration()
+            {
+                Metadata = new PeerMetadata(new Dictionary<string, string>()
+                {
+                    ["name"] = peerName,
+                })
+            });
+        }
+
+        public static IPeer CreatePeer(this IServiceBus bus, string peerName, Dictionary<string, string> attributes)
+        {
+            attributes.Remove("name");
+
+            return bus.CreatePeer(new PeerConfiguration()
+            {
+                Metadata = new PeerMetadata(new Dictionary<string, string>()
+                {
+                    ["name"] = peerName,
+                }.Concat(attributes).ToDictionary(p => p.Key, p => p.Value))
+            });
+        }
+
+        public static string GetName(this IPeer peer)
+        {
+            return peer.Identity.Metadata.Get("name") ?? string.Empty;
         }
     }
 }
